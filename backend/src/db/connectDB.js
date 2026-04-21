1e6317612cd3da1de80b09461c4ed3832d2579a6
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-04-21 06:18:51 +0600

    refactor: Solve the problem when signup

--- backend/src/db/connectDB.js
+++ backend/src/db/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect(`${process.env.MONGODB_URL}/smartgpt`);
+    mongoose.connect(process.env.MONGODB_URL);
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
